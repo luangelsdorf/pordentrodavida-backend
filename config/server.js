2fd539078d05c766a559f064b51cbc8cd8ad008b
lulaluano <luanferreira2136@gmail.com>
2021-03-21 22:21:28 -0300

    initial commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '0ec2b02397a0ca497e096f9eebedc258'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '47999061-004b-4626-843d-80090b9cac69'
+};
